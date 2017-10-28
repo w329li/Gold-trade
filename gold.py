5ad6faa2bacd75a9f7d3a38fc75d7b90dc43ab8d
w329li <w329li@student.cs.uwaterloo.ca>
2017-10-28 15:07:09 -0400

    class added to gold.py

--- gold.py
+++ gold.py
@@ -4,6 +4,29 @@ import urllib, json, urllib2, requests
 import time
 import datetime as dt
 
+
+#####define customer class first
+
+
+
+####define the customer class
+class Customer:
+    '''Fields: userID(Str),password(Str),gold(float),deposit(float)'''
+    def __init__(self,id,password,gold,money):
+                self.userID = id
+                self.password = password
+                self.gold = gold
+                self.deposit = money
+
+                print("now create a customer")
+                name = input("please input userID")
+                while (true):
+                    password = input("please input password")
+                    recheck = input("please re-input password")
+                    if (password == recheck): break
+                    else: print("password is wrong!")
+
+
 data = {}
 data["appkey"] = "8a815caa8cc5e08c"
 
@@ -18,3 +41,6 @@ while(i < 5):
   time.sleep(5)
   print(result["result"][0]["price"])
   i += 1
+
+
+
