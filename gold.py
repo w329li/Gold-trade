6c15fd2434e60ce3685b20f6a23205f4dcd99369
Zihao Fang <z27fang@csclub.uwaterloo.ca>
2017-10-28 14:43:27 -0400

    added timer features to gold.py

--- gold.py
+++ gold.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # encoding:utf-8
 import urllib, json, urllib2, requests
+import time
+import datetime as dt
 
 data = {}
 data["appkey"] = "8a815caa8cc5e08c"
@@ -10,6 +12,13 @@ url = "http://api.jisuapi.com/gold/shgold"+"?"+url_value
 request = urllib2.Request(url)
 result = urllib2.urlopen(request)
 jsonarr = json.loads(result.read())
-
 print(jsonarr)
+
+start_time = dt.datetime.today().timestamp()
+i = 0
+while(i < 5):
+  time.sleep(5)
+  print(i)
+  i += 1
+
 
